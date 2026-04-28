b189a6c7e22522ac381aeae5028e535271176768
yash shukla <yashshuklashuklas6@gmail.com>
2026-04-28 20:38:03 +0530

    Fix API URL

--- my-project/src/api/axios.js
+++ my-project/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://logistics-cargo-management-system.onrender.com/api/v1",
   withCredentials: true,
 });
 
