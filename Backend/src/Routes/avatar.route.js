fff062a5ca50656d044e2432a3e45f067c3a74e6
yash shukla <yashshuklashuklas6@gmail.com>
2026-04-28 20:02:48 +0530

    sd

--- Backend/src/Routes/avatar.route.js
+++ Backend/src/Routes/avatar.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteAvatar } from "../Controllers/updateuserinfo.controller.js";
+import { deleteAvatar } from "../controllers/updateuserinfo.controller.js";
 import { verifyJWT } from "../middleware/Auth.middleware.js";
 
 const router = express.Router();
